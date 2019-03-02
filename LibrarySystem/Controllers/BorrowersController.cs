86e2af7895b87c038a93e68a2d8d285043059152
Rupinder Singh <RS044@atcltd.atc.org.nz>
2019-03-02 13:18:17 +1300

    Add project files.

---
+++ LibrarySystem/Controllers/BorrowersController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LibrarySystem;
+
+namespace LibrarySystem.Controllers
+{
+    public class BorrowersController : Controller
+    {
+        private LibrarySystemEntities2 db = new LibrarySystemEntities2();
+
+        // GET: Borrowers
+        public ActionResult Index()
+        {
+            return View(db.Borrowers.ToList());
+        }
+
+        // GET: Borrowers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Borrower borrower = db.Borrowers.Find(id);
+            if (borrower == null)
+            {
+                return HttpNotFound();
+            }
+            return View(borrower);
+        }
+
+        // GET: Borrowers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Borrowers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "BorrowerID,BorrowerName,Address,Phone")] Borrower borrower)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Borrowers.Add(borrower);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(borrower);
+        }
+
+        // GET: Borrowers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Borrower borrower = db.Borrowers.Find(id);
+            if (borrower == null)
+            {
+                return HttpNotFound();
+            }
+            return View(borrower);
+        }
+
+        // POST: Borrowers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "BorrowerID,BorrowerName,Address,Phone")] Borrower borrower)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(borrower).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(borrower);
+        }
+
+        // GET: Borrowers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Borrower borrower = db.Borrowers.Find(id);
+            if (borrower == null)
+            {
+                return HttpNotFound();
+            }
+            return View(borrower);
+        }
+
+        // POST: Borrowers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Borrower borrower = db.Borrowers.Find(id);
+            db.Borrowers.Remove(borrower);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ LibrarySystem/Controllers/LibrariesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LibrarySystem;
+
+namespace LibrarySystem.Controllers
+{
+    public class LibrariesController : Controller
+    {
+        private LibrarySystemEntities2 db = new LibrarySystemEntities2();
+
+        // GET: Libraries
+        public ActionResult Index()
+        {
+            return View(db.Libraries.ToList());
+        }
+
+        // GET: Libraries/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Library library = db.Libraries.Find(id);
+            if (library == null)
+            {
+                return HttpNotFound();
+            }
+            return View(library);
+        }
+
+        // GET: Libraries/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Libraries/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Itemid,ItemType,Title,Author,Edition,ISBNumber,ItemStatus,LibraianDetails,StaffDetails")] Library library)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Libraries.Add(library);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(library);
+        }
+
+        // GET: Libraries/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Library library = db.Libraries.Find(id);
+            if (library == null)
+            {
+                return HttpNotFound();
+            }
+            return View(library);
+        }
+
+        // POST: Libraries/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Itemid,ItemType,Title,Author,Edition,ISBNumber,ItemStatus,LibraianDetails,StaffDetails")] Library library)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(library).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(library);
+        }
+
+        // GET: Libraries/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Library library = db.Libraries.Find(id);
+            if (library == null)
+            {
+                return HttpNotFound();
+            }
+            return View(library);
+        }
+
+        // POST: Libraries/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Library library = db.Libraries.Find(id);
+            db.Libraries.Remove(library);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
